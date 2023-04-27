57b1c1572d332757274974e0ea64ea6f0c7950b5
piyushdubey <piyushdubey07@gmail.com>
2023-04-26 23:57:32 -0400

    Adding more brews and taking it slow with the hyperaliasing.. It's mad AF

--- utils/install.sh
+++ utils/install.sh
@@ -4,79 +4,86 @@
 printf '\nInstalling development packages...\n'
 
 # Javascript development
-brew install \
-    jq \
-    node \
-    nvm \
-    yarn \
-    htop
+brew install jq
+brew install node
+brew install nvm
+brew install yarn
+brew install htop
 mkdir -p ~/.nvm
 
 # Python development
-brew install \
-    python3 \
+brew install python3
 
 # Go development
-brew install \
-    go \
+brew install go
 
 # IDEs and other guis
-brew install --cask \
-    mysql-shell \
-    quicklook-json \
-    sequel-ace \
-    visual-studio-code
-
-brew install --cask \
-    docker
+brew install --cask mysql-shell
+brew install --cask quicklook-json
+brew install --cask sequel-ace
+brew install --cask visual-studio-code
+brew install --cask docker
 
 # Compilers, and clis.
-brew install \
-    awscli \
-    gcc \
-    gettext \
-    pkg-config \
-    supabase/tap/supabase \
-    s3cmd
+brew install awscli
+brew install gcc
+brew install gettext
+brew install pkg-config
+brew install supabase/tap/supabase
+brew install s3cmd
+
 brew link --force gettext
 
 # Install things we use for collaboration and productivity.
 printf '\nInstalling collaboration and productivity packages...\n'
 
 # Browsers
-brew install --cask \
-    firefox \
-    google-chrome
+brew install --cask firefox
+brew install --cask google-chrome
 
 # Productivity boosters
-brew install \
-    speedtest-cli \
-    tree \
-    wget \
-    fig
+brew install speedtest-cli
+brew install tree
+brew install wget
+brew install fig
+
+## Productivity Tools
 
 # Calibre
-brew install --cask \
-    calibre \
-    notion
+brew install --cask calibre
+
+# Notion
+brew install --cask notion
+
+# Microsoft Office
+brew install --cask microsoft-office
+
+# Mega Sync
+brew install --cask megasync
+
+# Dropbox
+brew install --cask dropbox
+
+# Google Drive
+brew install --cask google-drive
 
 # Tool chain
-brew install --cask \
-    flux \
-    keepingyouawake \
-    rectangle \
-    yubico-yubikey-manager \
-    maccy
+brew install --cask flux 
+brew install --cask keepingyouawake 
+brew install --cask rectangle
+brew install --cask yubico-yubikey-manager 
+brew install --cask maccy
+brew install --cask vlc
+brew install --cask sourcetree
 
 brew tap homebrew/cask-drivers
 brew install --cask yubico-authenticator
 brew install --cask bitwarden
 
 # Collaboration packages
-brew install --cask \
-    discord \
-    slack \
-    zoom
+brew install --cask discord 
+brew install --cask slack
+brew install --cask zoom
 
 npm i -g \
     aws-cdk
